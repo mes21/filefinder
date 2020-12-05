b440a9532a60d5d70c0014b3a3ca4a243729ea86
mes21 <60403380+mes21@users.noreply.github.com>
2020-12-05 18:56:14 +0100

    update namefilter and concept

--- filefinder.py
+++ filefinder.py
@@ -3,14 +3,67 @@ import os
 import time
 import concurrent.futures
 
+def ext_filter(check_str, matchlist, exclude=False):
 
+    # add "." if missing
+    checks_list_ = []
+    for ext in matchlist:
+        if isinstance(ext, str):
+            if ext[0] != ".":
+                checks_list_.append("." + ext)
+            else:
+                checks_list_.append(ext)
 
+    if exclude is False:
+        for check in checks_list_:
+            print(check, check_str)
+            if check == check_str:
+                return True
+        return False
 
+    else:
+        for check in checks_list_:
+            if check == check_str:
+                return False
+        return True
 
+def or_filter(check_str, matchlist, exclude=False):
+
+    if exclude is False:
+        for check in matchlist:
+            if check in check_str:
+                return False
+        return True
+
+    else:
+        for check in matchlist:
+            if check in check_str:
+                return True
+        return False
+
+def and_filter(check_str, matchlist, exclude=False):
+
+    if exclude is False:
+        for check in matchlist:
+            if check not in check_str:
+                return False
+        return True
+
+    else:
+        for check in matchlist:
+            if check in check_str:
+                return False
+        return True
+
+def strtolist(input_):
+    if isinstance(input_, str):
+        return [input_]
+    else:
+        return input_
 
 
 class FileFinder():
-    def __init__(self, path, local=True, zip=False):
+    def __init__(self, path, local=True, zip=False, bucket=None, aws_cred=None):
 
         #print(path)
         #print(os.listdir(path))
@@ -19,32 +72,26 @@ class FileFinder():
         self.match_list = []
 
         dir_list = []
+        if bucket is None:
+            if local is True:
+                for root, dirs, files in os.walk(path):
+                    for name in files:
+                        self.raw_list.append(os.path.join(root, name))
+                    for name in dirs:
+                        if os.path.isfile(os.path.join(root, name)):
+                            self.raw_list.append(os.path.join(root, name))
+
+            # use threading for network drives
+            else:
+
+                self.__list(path)
         
-        if local is True:
-            for root, dirs, files in os.walk(test_path):
-                for name in files:
-                    raw_list.append(os.path.join(root, name))
-                for name in dirs:
-                    if os.path.isfile(os.path.join(root, name)):
-                        raw_list.append(os.path.join(root, name))
-
-        # use threading for network drives
+        # get list of AWS Bucket
         else:
+            pass
 
-            for el in os.listdir(path):
-                el_ = os.path.join(path, el)
-                
-                #print("el:", el_)
-                if os.path.isdir(el_) is True:
-                    dir_list.append(el_)
-                else:
-                    self.raw_list.append(el_)
 
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-
-                results = executor.map(self.__list, dir_list)
-                #for res in results:
-                #    pass
+        self.match_list = self.raw_list.copy()
 
     def __list(self, path):
 
@@ -62,61 +109,29 @@ class FileFinder():
             #for res in results:
             #    pass
 
-    def __getchecklist(self):
-
-        if len(self.match_list) == 0:
-            return self.raw_list
-        else:
-            return self.match_list
-
-    def __strtolist(self, input_):
-        if isinstance(input_, str):
-            return [input_]
-        else:
-            return input_
 
 
-    def __updatechecklist(self, checklist_, match_list, match):
 
-        if match is True:
-            return match_list
-               
-        else:
-            for el in match_list:
-                if el in checklist_:
-                    checklist_.remove(el)
- 
-            return checklist_
+    def namefilter(self, path_and=None, path_or=None, file_and=None, file_or=None, file_ext=None, exclude=False):
+
+        if file_ext:
+            self.match_list = [x for x in self.match_list if ext_filter(os.path.splitext(x)[1], strtolist(file_ext), exclude)]
+
+        if path_or:
+            self.match_list = [x for x in self.match_list if or_filter(os.path.split(x)[0], strtolist(path_or), exclude)]
+
+        if path_and:
+            self.match_list = [x for x in self.match_list if and_filter(os.path.split(x)[0], strtolist(path_and), exclude)]
+
+        if file_or:
+            self.match_list = [x for x in self.match_list if or_filter(os.path.split(x)[1], strtolist(file_or), exclude)]
+
+        if file_and:
+            self.match_list = [x for x in self.match_list if and_filter(os.path.split(x)[1], strtolist(file_and), exclude)]
 
 
-    def namefilter(self, path_and=None, path_or=None, file_and=None, file_or=None, file_ext=None, match=True):
-
-        new_match_list = []
-
-        check_list = self.__getchecklist()
-
-        if file_ext is not None:
-            match_list = []
-            ext_list = self.__strtolist(file_ext)
-            checks_list_ = []
-            # add "." if missing
-            for ext in ext_list:
-                if isinstance(ext, str):
-                    if ext[0] != ".":
-                        checks_list_.append("." + ext)
-                    else:
-                        checks_list_.append(ext)
-
-            # check for matches
-            for file_ in check_list:
-                file_ext = os.path.splitext(file_)[1]
-                
-                if file_ext in checks_list_:
-                    match_list.append(file_)
-
-        check_list = self.__updatechecklist(check_list, match_list, match)
-        print("check_list", check_list)
-        self.match_list = check_list
+        #print("check_list", self.__getchecklist())
+        #self.match_list = check_list
 
 
 
@@ -125,6 +140,10 @@ class FileFinder():
 
     def datefilter(self, date, operator):
         pass
+
+    def resetfilter(self):
+        self.match_list = self.raw_list.copy()
+
 
     def result(self):
         return self.match_list
@@ -141,8 +160,7 @@ if __name__ == '__main__':
 
 
     f = FileFinder(test_path, local=False)
-    print("RES:")
-    f.namefilter(file_ext="zip", match=False)
+    f.namefilter(file_ext="zip", exclude=False)
     res = f.result()
     print(len(res))
     print("res:", res)
