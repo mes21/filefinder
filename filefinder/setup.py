6fb50cf21a2280dd307cb8382157543b38e334d0
mes21 <60403380+mes21@users.noreply.github.com>
2021-01-01 19:46:48 +0100

    make vaild setup

---
+++ filefinder/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='filefinder',
+    version='0.1.0',    
+    description='python class for searching files on harddrive',
+    url='https://github.com/mes21/filefinder',
+    author='Tobias Andorfer',
+    author_email='123fakestreet@gmail.com',
+    license='MIT License',
+    packages=['filefinder'],
+    install_requires=[                    
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',     
+        'Programming Language :: Python :: 3.7',
+    ],
+)
